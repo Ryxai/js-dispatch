c168b5c7b79e4d3181c415c73076beee6f82cf8d
Ryxai <jmichaelbaum@gmail.com>
2021-12-23 13:01:26 -0500

    Adjujsted formulation for jsrsasign, previously broken implementation. Additionally, started crafting a better workflow

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,19 @@ import express from "express";
 import sign from "jsrsasign";
 import config from "config";
 import fs from "fs";
-
+import axios from "axios";
+import bp from "body-parser";
 import {Mapping} from "./main";
 
+const OAUTH_SUCCESS : string = "ouath_success";
+
+const pb = fs.readFileSync("key","binary");
 console.log("Initializing");
 const port : number = config.get('port');
 const app = express();
-const publicKey : string = config.get('publicKey');
+app.use(bp.json());
+app.use(bp.urlencoded({extended: true}));
+const publicKey = sign.KEYUTIL.getKey(pb);
 let hash_alg : string = config.get('hash_alg');
 if (!config.has('map_config')){
   throw "No configuration mapping"
@@ -20,21 +26,42 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-route_config.map(({route, file_path }) => (app.get(route, (req, res) => {
+app.get(`${process.env.GOOGLE_AUTH_ROUTE}`, (req, res) => {
+  axios.post("https://oauth2.googleapis.com/token", {
+        ...req.body,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        redirect_url: `${process.env.API_URL}/${OAUTH_SUCCESS}`
+      }
+  ).then((r) => {
+    req.body["grant_types"] === "authorization_code"
+    ? axios.get(`https://www.googleapis.com/oauth2/v2/userinfo?access_token=${r.data.access_token}`).then((u) =>
+        {
+          res.status(200).send({...r.data,label:u.data.email})
+        }):
+        res.status(200).send(r.data);
+  }).catch((e) => {
+    res.status(500).send(e.response?.data || {message: e.message});
+    })
+});
+app.get(OAUTH_SUCCESS,(req, res) => {
+  res.send(`${OAUTH_SUCCESS}.html`);
+})
+route_config.map(({route, file_path }) => (app.post(route, (req, res) => {
   if (route == "/"){
     res.status(200).send(files[file_path]);
   }
   else {
-    const sig_text : string = req.body["auth"];
+    let sig_text : string = req.body.auth;
     if ('alg' in req.body) {
-      hash_alg = req.body["alg"];
+      hash_alg = req.body.alg;
     }
-    const pub = sign.KEYUTIL.getKey(publicKey);
-    const text = `${file_path}${hash_alg}`;
+    const date : Date = new Date();
+    const text = `${date.getDay()}${date.getHours()}`;
     const sig = new sign.KJUR.crypto.Signature({alg: hash_alg});
-    sig.init(pub);
+    sig.init(publicKey);
     sig.updateString(text);
-    if (sig.verify(sign.stohex(sig_text))) {
+    if (sig.verify(sig_text)) {
       res.send(files[file_path]);
     }
     else {
@@ -42,6 +69,9 @@ route_config.map(({route, file_path }) => (app.get(route, (req, res) => {
     }
   }
 })));
+app.get("*", (req, res) =>{
+  res.send("index");
+})
 
 
 
