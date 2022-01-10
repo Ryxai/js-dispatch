bd62735cb94985f5aaa69438a5f7723f10324659
Ryxai <jmichaelbaum@gmail.com>
2022-01-10 03:54:16 -0500

    Major rebuild of app structure

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import config from "config";
-import fs from "fs";
 import axios from "axios";
 import bp from "body-parser";
 import {Mapping} from "./main";
@@ -12,7 +11,6 @@ const port : number = config.get('port');
 const app = express();
 app.use(bp.json());
 app.use(bp.urlencoded({extended: true}));
-let hash_alg : string = config.get('hash_alg');
 if (!config.has('google_auth_route')){
   throw "No auth route"
 }
@@ -20,10 +18,9 @@ const googleAuthRoute : string = config.get('google_auth_route');
 const googleClientID : string = config.get('google_client_id');
 const googleClientSecret : string = config.get("google_client_secret");
 const apiUrl : string = config.get("api_url");
-const oauthSuccessRoute : string = "ouath_success";
+const oauthSuccessRoute : string = "oauth_success";
 
 //Logging
-console.log(`Hash alg: ${hash_alg}`);
 console.log(`Google Route: ${googleAuthRoute}`);
 console.log(`Google token generator` );
 console.log(`Google client id: ${googleClientID}`);
@@ -35,32 +32,29 @@ app.listen(port, () => {
 });
 
 //Handling default public
-console.log("mapping default routes")
-app.get("index",(req,res) => {
-  res.sendFile("index.html");
+console.log("mapping standard routes");
+app.get("/",(req,res) => {
+   res.sendFile(`${__dirname}/public/index.html`);
 });
-app.get(`${oauthSuccessRoute}`,(req, res) => {
-  res.sendFile("ouath_success.html");
-})
+app.get(`/${oauthSuccessRoute}`,(req, res) => {
+  res.sendFile(`${__dirname}/public/oauth_success.html`);
+});
 
 //Importing mappings and handling public
+console.log("Setting up routes for mappings from the config file");
 if (!config.has('map_config')){
-  throw "No configuration mapping"
+  throw "No configuration mapping";
 }
 const {mappings : route_config } : {mappings: Mapping[]} = JSON.parse(config.get("map_config"));
-const files: {[file_path: string]: string} = {};
-route_config.map(({file_path}) => (files[file_path.toString()] = fs.readFileSync(file_path, "utf-8")));
-route_config.map(({route, file_path}) => console.log(`${route}:${file_path}`));
-route_config.map(({route, file_path }) => (app.post(route, (req, res) => {
-  console.log(`Request for ${route} from ${req.ip}`);
-  res.send(files[file_path]);
+route_config.map(({route, file_path }) => (app.get(route, (req, res) => {
+  console.log(`Request from ${req.ip} for ${file_path}`);
+  res.sendFile(`${__dirname}/${file_path}`);
 })));
 
 
-console.log("Route configurations");
-console.log(`${googleAuthRoute}: google`)
-app.get(googleAuthRoute, (req, res) => {
-  console.log(`Oauth request from ${req.ips.join(" ")}`)
+console.log(`Setting up oauth route ${googleAuthRoute}: google`);
+app.get(`/${googleAuthRoute}`, (req, res) => {
+  console.log(`Oauth request from ${req.ip}`);
   const postData = {
     ...req.body,
     client_id: googleClientID,
@@ -75,20 +69,11 @@ app.get(googleAuthRoute, (req, res) => {
           res.status(200).send({...r.data,label:u.data.email})
         }):
         res.status(200).send(r.data);
-  }).catch((e) => {
-    res.status(500).send(e.response?.data || {message: e.message});
-    });
-});
-console.log(`${oauthSuccessRoute}: oauth_success`);
-app.get(oauthSuccessRoute,(req, res) => {
-  res.sendFile(`${oauthSuccessRoute}`);
+  }).catch((e) =>
+    {res.status(500).send(e.response?.data || {message: e.message});}
+  );
 });
 
-//Generic catch all
-console.log("All others: index");
-app.get("*", (req, res) =>{
-  res.send("index");
+app.get("*",(req,res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
 });
-
-
-
